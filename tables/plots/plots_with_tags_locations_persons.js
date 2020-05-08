749f4b2d32fd84929f7c8e6e921a3a9f89a84928
YulyKo <juliakotenko.forstudy@gmail.com>
2020-05-08 19:21:11 +0300

    fix compare JSON for plots

--- tables/plots/plots_with_tags_locations_persons.js
+++ tables/plots/plots_with_tags_locations_persons.js
@@ -9,7 +9,7 @@ const REQUEST_PLOT_TAGS = `SELECT plot_tag.id_plot, tags.id, tags.name, tags.bg_
 FROM plot_tag JOIN tags ON tags.id = plot_tag.id_tag`;
 const REQUEST_PLOT_PERSONS = `SELECT plot_person.id_plot, persons.id, persons.name FROM plot_person 
 Left JOIN persons ON persons.id = plot_person.id_person`;
-const REQUEST_PLOT_LOCATIONS = `SELECT plot_location.id_plot, plot_location.id_location , locations.name, locations.photo_url, locations.author 
+const REQUEST_PLOT_LOCATIONS = `SELECT plot_location.id_plot, plot_location.id_location, locations.name, locations.photo_url, locations.author 
 FROM plot_location LEFT JOIN locations ON locations.id = plot_location.id_location`;
 
 const getPlotsWithTagsPersonsAndLocations = (request, response) => {
@@ -65,45 +65,52 @@ function comparePlotInfoToJSON(plots) {
 }
 
 function comparePlotLocationsToJSON(locations) {    
-    for (let index = 0; index < locations.length; index++) {        
-        if (+plotsJSON[index].data.id === +locations[index].id_plot) {
-            let item = {
-                "id": locations[index].id,
-                "name": locations[index].name,
-                "photo_url": locations[index].photo_url,
-                "author": locations[index].author, 
-            };
-            plotsJSON[index].locations.push(item)
-        }
+  for (let index_plot = 0; index_plot < plotsJSON.length; index_plot++) {
+    for (let index_location = 0; index_location < locations.length; index_location++) {
+      if (+plotsJSON[index_plot].data.id === +locations[index_location].id_plot) {
+        let item = {
+          "id": locations[index_location].id,
+          "name": locations[index_location].name,
+          "photo_url": locations[index_location].photo_url,
+          "author": locations[index_location].author,
+        };
+        plotsJSON[index_plot].locations.push(item)
+      }
+
     }
+  }
 }
 
 function comparePlotPersonsToJSON(persons) {    
-    for (let index = 0; index < persons.length; index++) {        
-        if (+plotsJSON[index].data.id === +persons[index].id_plot) {
-            let item = {
-                "id": persons[index].id,
-                "name": persons[index].name
-            };
-            plotsJSON[index].persons.push(item)
+    for (let index_plot = 0; index_plot < plotsJSON.length; index_plot++) {
+      for (let index_person = 0; index_person < persons.length; index_person++ ) {
+        if (+plotsJSON[index_plot].data.id === +persons[index_person].id_plot) {
+          let item = {
+            "id": persons[index_person].id,
+            "name": persons[index_person].name
+          };
+
+          plotsJSON[index_plot].persons.push(item)
         }
+      }
     }
 }
 
 function comparePlotTagsToJSON(tags) {
-    for (let index = 0; index < tags.length; index++) {
-        if (+plotsJSON[index].data.id === +tags[index].id_plot) {
-            let item = {
-                "id": tags[index].id,
-                "name": tags[index].name,
-                "bg_color": tags[index].bg_color,
-                "text_color": tags[index].text_color, 
-            };
-            plotsJSON[index].tags.push(item)
-        }
+  for (let index_plot = 0; index_plot < plotsJSON.length; index_plot++) {
+    for (let index_tag = 0; index_tag < tags.length; index_tag++) {
+      if (+plotsJSON[index_plot].data.id === +tags[index_tag].id_plot) {
+        let item = {
+          "id": tags[index_tag].id,
+          "name": tags[index_tag].name,
+          "bg_color": tags[index_tag].bg_color,
+          "text_color": tags[index_tag].text_color,
+        };
+        plotsJSON[index_plot].tags.push(item)
+      }
     }
+  }
 }
-
 
 module.exports = {
     name,
