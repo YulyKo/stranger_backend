ff98187bffae752b5bf3dfc9528539f58263cbef
YulyKo <juliakotenko.forstudy@gmail.com>
2020-05-28 22:58:27 +0300

    fix deleting location

--- tables/locations.js
+++ tables/locations.js
@@ -1,10 +1,11 @@
-const db_propertis = require('../db_properties');
+const db_properties = require('../db_properties');
+const _by_id = require('./../common/_by_id')
 
 const name = 'locations';
 
 const post = (request, response) => {
   const { author, name, photo_url } = request.body;
-  db_propertis.pool.query(
+  db_properties.pool.query(
     'INSERT INTO locations( author, name, photo_url ) VALUES ($1, $2, $3)',
     [author, name, photo_url], (error, results) => {
       if (error) {
@@ -18,21 +19,30 @@ const post = (request, response) => {
 
 const del = (request, response) => {
   const id = parseInt(request.params.id)
-
-  db_propertis.pool.query('DELETE FROM locations WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`location deleted with ID: ${id}`)
-  });
+  deleteLocationFromTable(id)
+  console.log('location' + id);
 };
 
+function deleteLocationFromTable(id) {
+  db_properties.pool.query(`DELETE FROM plot_location WHERE id_location = $1;`, [id], (error) => {
+    if (error) { throw error }
+  });
+  deleteFromPlotLocationTable(id)
+  console.log('location ' + id);
+}
+
+function deleteFromPlotLocationTable(id) {
+  db_properties.pool.query(`DELETE FROM locations WHERE id = $1;`, [id], (error) => {
+    if (error) { throw error }
+    console.log('location ' + id);
+  });
+}
 
 const update = (request, response) => {
   const id = parseInt(request.params, id)
   const { author, name, photo_url } = request.body
 // author, name, photo_url
-  db_propertis.pool.query(
+  db_properties.pool.query(
     'UPDATE locations SET author = $1, name = $2, photo_url = $3 WHERE id = $4',
     [author, name, photo_url, id],
     (error, results) => {
@@ -47,7 +57,7 @@ const update = (request, response) => {
 const get = (request, response) => {
   console.log('get locations');
 
-  db_propertis.pool.query('SELECT * FROM locations', (error, results) => {
+  db_properties.pool.query('SELECT * FROM locations', (error, results) => {
     if(error) { throw error }
     response.status(200).json(results.rows)
   });
@@ -57,7 +67,7 @@ const getById = (request, response) => {
   console.log('location get by id');
   
   const id = parseInt(request.params.id);
-  db_propertis.pool.query('SELECT * FROM locations WHERE id = $1', [id], (error, result) => {
+  db_properties.pool.query('SELECT * FROM locations WHERE id = $1', [id], (error, result) => {
     if (error) { throw error }
     response.status(200).json(result.rows);
   });
