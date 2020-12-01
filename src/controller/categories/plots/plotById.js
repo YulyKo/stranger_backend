20fe0db6aa4457afd4d250f323d92d46e6b0898a
YulyKo <yuliakotenko1.0@gmail.com>
2020-12-01 22:19:56 +0200

    plot/added GET tags by plot.id

--- src/controller/categories/plots/plotById.js
+++ src/controller/categories/plots/plotById.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
 const logger = require('../../../utils/logger')(__filename);
 const createError = require('http-errors');
-const { categories: { plot } } = require('../../../services');
+const { categories: { plot, tag } } = require('../../../services');
 
 const get = async (req, res, next) => {
   try {
     const id = req.params.id;
     logger.info(`get plot by id ${id}`);
-    const plotInfo = await plot.getById(id);
-    res.status(200).send(plotInfo);
+    // const plotInfo = await plot.getById(id);
+    const tags = await tag.getForPlot(id);
+    res.status(200).send(tags);
   } catch (error) {
     logger.error(error.message);
     next(createError(500, error.message));
--- src/services/categories/tag.js
+++ src/services/categories/tag.js
@@ -12,6 +12,20 @@ const getTagsForPlots = async () => {
     return tags;
 };
 
+const getForPlot = async (id) => {
+  const tags = await knex('plot_tag')
+    .select(
+      'tags.id',
+      'tags.name',
+      'tags.bg_color',
+      'tags.text_color'
+    )
+    .leftJoin('tags', { 'tags.id': 'plot_tag.id_tag' })
+    .where({ 'plot_tag.id_plot': id });
+  return tags;
+};
+
 module.exports = {
   getTagsForPlots,
+  getForPlot,
 };
